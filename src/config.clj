02005bedf4ecda0028eef6dfe3c5582307de9ed5
inukahbhsekum <mukeshbhakuni.almora@gmail.com>
2024-09-22 17:41:54 +0530

    Add initial project structure
    + Add directory structure required for project
    + Add code to read db config and other settings
      from config.edn
    + update docker-compose.yml for username and
      password
    + Add initial configuration settings in config.edn

---
+++ src/config.clj
@@ -0,0 +1,10 @@
+(ns notification-service.config
+ (:require [aero.core :as aero]
+  [clojure.java.io :as io]))
+
+
+(defn read-config
+ []
+ (-> "config.edn"
+  (io/resource)
+  (aero/read-config)))
