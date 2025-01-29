e65aea250b2ded813c1246fe5c17b102338d8569
mukeshbhakuni <mukesh@helpshift.com>
2025-01-29 23:54:14 +0530

    Add model functions for message-activity-log
    + Add function in models layer for updating
      activity-log for tracking status
    + Add id as primary key in message-activity-log

--- src/messages/models.clj
+++ src/messages/models.clj
@@ -42,22 +42,6 @@
       (ur/failed message-payload))))
 
 
-(defn fetch-user-messages-for-topic
-  [{:keys [topic-id] :as zmap} {:keys [db-pool]}]
-  (try
-    (let [query (-> {:select [:*]
-                     :from [:notification_message]
-                     :where [:= :topic_id (UUID/fromString topic-id)]}
-                    (sql/format {:pretty true}))
-          topic-messages (jdbc/execute-one! (db-pool)
-                                            query
-                                            {:builder-fn rs/as-unqualified-kebab-maps})]
-      topic-messages)
-    (catch Exception e
-      (ctl/error "User not found " (ex-message e))
-      (throw (Exception. "Invalid user-id")))))
-
-
 (defn fetch-user-pending-messages-for-topic
   [{:keys [topic-id user-id] :as zmap} {:keys [db-pool]}]
   (try
@@ -92,6 +76,34 @@
 
 
 (defn update-message-activity-log
-  [message {:keys [db-pool]}]
-  ;; TODO: complete the activity log for the actions
-  ())
+  [payload {:keys [db-pool]}]
+  (let [id (UUID/randomUUID)
+        query (-> {:insert-into   [:notification_message_activity_log]
+                   :columns       [:id :message_id :topic_id :sender
+                                   :receiver :meta :action_taken_at
+                                   :created_at]
+                   :values        [[id
+                                    (:message-id payload)
+                                    (:topic_id payload)
+                                    (:sender_id payload)
+                                    (:receiver payload)
+                                    (:meta payload)
+                                    (ctco/to-sql-time (ctc/now))
+                                    (ctco/to-sql-time (ctc/now))]]
+                   :on-conflict   [:message_id
+                                   {:where [:<> :message_id nil]}]
+                   :do-update-set {:fields [:id
+                                            :message_id
+                                            :topic_id
+                                            :sender
+                                            :receiver
+                                            :meta
+                                            :action_taken_at
+                                            :created_at]}}
+                  (sql/format {:pretty true}))
+        message-details (jdbc/execute-one! (db-pool)
+                                           query
+                                           {:builder-fn rs/as-unqualified-kebab-maps})]
+    (if message-details
+      (ur/created payload)
+      (ur/failed payload))))
